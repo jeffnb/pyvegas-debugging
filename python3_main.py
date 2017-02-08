528102a62d2ef4003d9f7a36da439129361b339b
Jeff <jeff@goproject100.com>
2017-02-07 16:43:44 -0800

    Changing for profiling

--- python3_main.py
+++ python3_main.py
@@ -4,13 +4,13 @@ if __name__ == '__main__':
     """
     An imperfect matcher
     """
-    print("Please enter the letters:")
-    word_input = input()
 
-    print("Please enter length of words desired:")
-    find_length = int(input())
+    word_input = "magnificent"
+    find_length = 4
 
-    matches = find_matches("hello", find_length)
+    matches = find_matches(word_input, find_length)
 
+    print("Looking for scrambles of {} with length {}".format(word_input,
+                                                              find_length))
     print("Found {} matches".format(len(matches)))
     print(matches)
