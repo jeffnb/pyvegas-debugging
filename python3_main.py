61866c6c3c02a996f18f2903bc205db0596479b3
Jeff <jeff@goproject100.com>
2017-02-07 16:39:04 -0800

    Initial Commit

---
+++ python3_main.py
@@ -0,0 +1,16 @@
+from word_descrambler import find_matches
+
+if __name__ == '__main__':
+    """
+    An imperfect matcher
+    """
+    print("Please enter the letters:")
+    word_input = input()
+
+    print("Please enter length of words desired:")
+    find_length = int(input())
+
+    matches = find_matches(word_input, find_length)
+
+    print("Found {} matches".format(len(matches)))
+    print(matches)
---
+++ word_descrambler.py
@@ -0,0 +1,39 @@
+import re
+
+
+def is_match(word, scrambled):
+    """
+    Simply takes the word and determines if its letters are part of scrambled
+    :param word: word to try to match
+    :param scrambled: scrambled letters
+    :return: boolean
+    """
+    # Loop through the letters and make sure they all match
+    match = True
+    for letter in word:
+        if letter not in scrambled:
+            match = False
+
+    return match
+
+
+def find_matches(scrambled, find_len):
+    """
+    Opens the dictionary and loops through to find matches
+    :param scrambled: the letters to find words out of
+    :param find_len: the length of word to find
+    :return: list of matches
+    """
+    with open('/usr/share/dict/words') as word_file:
+        words = word_file.readlines()
+
+    # Strip all surrounding whitespace
+    words = [x.strip() for x in words]
+
+    # Find matching words
+    matches = []
+    for word in words:
+        if len(word) != find_len and is_match(word, scrambled):
+            matches.append(word)
+
+    return matches
